9a9f76edc0e69b55d5cc2cbb512e38c34a1de14c
Andre Vitorio <andre@vitorio.net>
2015-11-09 12:38:20 +0000

    Changed h1 text

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
   get '/' do
-    "<!DOCTYPE html><html><head></head><body><h1>Hello World</h1></body></html>"
+    "<!DOCTYPE html><html><head></head><body><h1>Is this just fantasy?</h1></body></html>"
   end
 end
